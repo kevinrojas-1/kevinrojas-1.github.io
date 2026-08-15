d0fbb1d748bdb34215b8032a08d095ed46103cd5
kevinrojas-1 <knrojasr@unbosque.edu.co>
2026-08-14 23:25:10 -0500

    Update site URL to use environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ const siteUrl = process.env.SITE_URL || homeData.siteUrl || undefined;
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://kevinrojas-1.github.io',
+  site: siteUrl,
   vite: {
     plugins: [tailwindcss()]
   },
